96fe167abee3ee5d34616481a57f1e26395546e6
Emmanuel Bouaziz <ebouaziz@neotion.com>
2015-04-20 15:05:40 +0200

    cleanup ticket validator

--- repository_hook_system/ticket_validator.py
+++ repository_hook_system/ticket_validator.py
@@ -6,16 +6,6 @@ from trac.core import Component, implements
 from trac.ticket.api import ITicketManipulator
 from trac.util.translation import tag_
 
-"""
-def validate_ticket(ticket):
-    if ticket['status'] == 'closed':
-        for c in forbidden_components_on_close:
-            if ticket['component'] == c:
-                yield 'component', tag_('Fix ticket component before closing')
-        for m in forbidden_milestones_on_close:
-            if ticket['milestone'] == m:
-                yield 'milestone', tag_('Fix ticket milestone before closing')
-"""
 
 class TicketChangeValidator(Component):
     #implements(ITicketManipulator, IRepositoryHookSubscriber)
@@ -47,43 +37,3 @@ class TicketChangeValidator(Component):
                 yield 'component', tag_('Fix ticket component before closing')
             if ticket['milestone'] in self._fbden_ms_on_close:
                 yield 'milestone', tag_('Fix ticket milestone before closing')
-
-    """
-    # IRepositoryHookSubscriber implementation
-    def is_available(self, repository, hookname):
-        return (hookname == 'pre-commit')
-
-    def invoke(self, project, repository, user, transaction, **kwargs):
-        PreCommitHook(self.env, project=project, rev=None,
-                      txn=transaction, rep=repository)
-    """
-
-"""
-class PreCommitHook(CommitHook):
-
-    def finalize(self, result):
-        if OK == result:
-            self._update_log(self.log)
-        raise HookStatus(result)
-
-    def _cmd_closes(self, ticket_id):
-        # simulate ticket close and call validator
-        ticket = Ticket(self.env, ticket_id)
-        ticket['status'] = 'closed'
-        ticket['resolution'] = 'fixed'
-
-        for f, m in validate_ticket(ticket):
-            print >> sys.stderr, m
-            self.finalize(ERROR)
-        return OK
-"""
-"""
-def toto(self):
-    field = 'reporter'
-    yield None, tag_("A ticket with the summary %(summary)s"
-                     " already exists.",
-                      summary=tag.em("Testing ticket manipulator"))
-    yield field, tag_("The ticket %(field)s is %(status)s.",
-                      field=tag.strong(field),
-                      status=tag.em("invalid"))
-"""
